1af4b30a25c01917a36446e8626c91e4f7de6ef6
Deluxear <89830002+Deluxear@users.noreply.github.com>
2021-12-18 01:02:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const Ano = 2021
+let NomeDaPessoa = prompt("Qual o seu nome?")
+let IdadeI = prompt ("Em qual ano você nasceu?") 
+let IdadeF = Ano - IdadeI;
+let mensagem = `Eita, parece que o ${NomeDaPessoa} quer encarar o campeão`;
+
+alert (mensagem);
+
+alert(`Seja bem Vindo ao meu site sobre o Jax, aqui você vai encontrar algumas informações sobre ele`)
