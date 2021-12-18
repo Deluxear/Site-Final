89c5909c0ead74148374676f9162de6cb1c9d783
Deluxear <89830002+Deluxear@users.noreply.github.com>
2021-12-18 15:37:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-const Ano = 2021
 let NomeDaPessoa = prompt("Qual o seu nome?")
-let IdadeI = prompt ("Em qual ano você nasceu?") 
-let IdadeF = Ano - IdadeI;
 let mensagem = `Eita, parece que o ${NomeDaPessoa} quer encarar o campeão`;
 
 alert (mensagem);
